ba09371d1e88ff4f25b8ab24a217adfc32c3f8d4
sabasallath <dan.le.blev@gmail.com>
2017-02-16 14:06:43 +0100

    pendu ++

--- pendu.sh
+++ pendu.sh
@@ -2,19 +2,47 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-ABC=$(echo {a..z})
-#vare="testvar"
-#echo ${vare%var}
+HANGCOUNT=0
+ATEMPT=7
+hang(){
+	((HANGCOUNT+=1))
+	echo "WRONG !!! $HANGCOUNT/$ATEMPT left"
+}
 
-WORD=$(compgen -ab | shuf -n 1̀)
+ABC=$(echo {a..z})
+
+while : ; do
+	WORD=$(compgen -ab | shuf -n 1̀)
+	[ ${#WORD} -lt 5 ] || break
+done
+
+# For cheater only
+# echo "word=$WORD"
 WORDLEFT="${WORD}"
 
-echo "${WORD}"
-echo "${ABC}"
-PS3=">>"
-prompt="sqs"
-echo -n "2/3 : "
-read LETTER
-echo "$LETTER"
-WORDLEFT="${WORD//$LETTER}"
-echo "$WORDLEFT"
+until [ "$WORDLEFT" == "" ] || [ "$HANGCOUNT" -gt "$ATEMPT" ]; do
+	echo -n "$WORD" | tr "$WORDLEFT" .
+	echo " ($((${#WORD}-${#WORDLEFT}))/${#WORD})"
+	read L
+	case ${#L} in 
+		1)
+			case "${WORD}" in
+				*"${L}"* )
+					WORDLEFT="${WORDLEFT//$L}";;
+				*) hang ;;
+			esac ;;
+		*)																	
+			if [ "$WORD" == "$L" ]; then
+				break
+			else 
+				hang
+			fi	;;
+	esac
+done
+
+if [ "$HANGCOUNT" -gt "$ATEMPT" ]; then
+	echo "You Loose ! The word was $WORD"
+else
+	echo "You Win !"
+fi 
+
